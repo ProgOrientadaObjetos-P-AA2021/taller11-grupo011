4a9b6b3ac638841966aa58421eac440ef2fd5efc
unknown <santiago9sr@hotmail.com>
2021-06-24 16:49:57 -0500

    a

--- Restaurante/src/restaurante/MenuEconomico.java
+++ Restaurante/src/restaurante/MenuEconomico.java
@@ -10,5 +10,33 @@ package restaurante;
  * @author usuario
  */
 public class MenuEconomico {
+    protected String nombrePlato;
+    protected double valorMenu;
+    protected double valorInicial;
+    protected double descuento;
     
+    public void establcerNombrePlato(String tipo){
+        nombrePlato = tipo;
+    }
+    public void establcerValorMenu(double tipo){
+        valorMenu = tipo;
+    }
+    public void establcerValorInicial(double tipo){
+        valorInicial = tipo;
+    }
+    public void establecerDescuento(double tipo){
+        descuento=tipo;
+    }
+    public String obtenerNombrePlato(){
+        return nombrePlato;
+    }
+    public double obtenerValorMenu(){
+        return valorMenu;
+    }
+    public double obtenerValorInicial(){
+        return valorInicial;
+    }
+    public double obtenerDescuento(){
+        return descuento;
+    }
 }
